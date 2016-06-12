3f5bee19f4d91df6a7b3833171bf28dd7b8e7d06
LuTri <tristan.lucas.de@gmail.com>
2016-06-12 22:49:12 +0200

    Enhance deploy-mc to get fetch / reset

--- apps/mc/management/commands/deploy-mc.py
+++ apps/mc/management/commands/deploy-mc.py
@@ -1,6 +1,8 @@
 from django.core.management import BaseCommand
 from django.core.management import CommandError
 
+from optparse import make_option
+
 import commands
 
 class RetryException(Exception):
@@ -8,6 +10,10 @@ class RetryException(Exception):
 
 class Command(BaseCommand):
 	max_cmd_tries = 10
+
+	option_list = BaseCommand.option_list + (
+		make_option('--head', action='store', dest='head', default='origin/master'),
+	)		
 
 	def __init__(self, *args, **kwargs):
 		self.verbose = True
@@ -23,6 +29,8 @@ class Command(BaseCommand):
 		while tries < 10:
 			try:
 				status, output = commands.getstatusoutput(cmd)
+				if self.verbose:
+					print output
 				if status != 0:
 					raise RetryException(output)
 				done = True
@@ -33,17 +41,24 @@ class Command(BaseCommand):
 
 		return done, output
 
-	def clean(self):
+	def fetch(self, head, **options):
+		opts = ['git', 'fetch', '&&', 'git', 'reset', '--hard', head, '&&']
+		opts += ['git', 'submodule', 'foreach', '--recursive', '"']
+		opts += ['git', 'fetch', '&&', 'git', 'reset', '--hard', 'origin/master', '"']
+		return self._run_shell_command(opts)
+
+	def clean(self, **options):
 		return self._run_shell_command(['make', 'clean'])
 
-	def make(self):
-		done, output = self.clean()
-		if done:
+	def make(self, **options):
+		d_fetch, ouoput = self.fetch(**options)
+		d_clean, output = self.clean(**options)
+		if d_fetch and d_clean:
 			return self._run_shell_command(['make'])
 		return done, output
 
-	def deploy(self):
-		done, output = self.make()
+	def deploy(self, **options):
+		done, output = self.make(**options)
 		if done:
 			return self._run_shell_command(['make', 'program'])
 		return done, output
@@ -53,8 +68,9 @@ class Command(BaseCommand):
 			action = "deploy"
 		else:
 			action = args[0]
+
 		try:
-			done, output = getattr(self, action)()
+			done, output = getattr(self, action)(**options)
 			if done:
 				print "Successfully finished command."
 			else:
