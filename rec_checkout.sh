6a894f6bc43bac29593bffdd271637ebbb7e4ab5
Tristan Lucas <tristan@adblockplus.org>
2018-04-11 13:19:22 +0200

    Also updating level 0 submodules

--- rec_checkout.sh
+++ rec_checkout.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
 git submodule foreach --recursive git fetch origin
 git submodule foreach --recursive git submodule init
 git submodule foreach --recursive git submodule update
