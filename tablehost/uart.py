47f5a7485af1c1ef3e0cd507fe5e08cf8269d90b
Tristan Lucas <tristan.lucas.de@gmail.com>
2018-04-12 12:05:11 +0200

    Uart WIP

--- tablehost/uart.py
+++ tablehost/uart.py
@@ -6,12 +6,19 @@ MAX_LEDS = 336
 class UartCom(object):
 	def __init__(self, debug=False, baud=500000):
 		self.baud = baud
-		self.data = [None] * MAX_LEDS
+		self.data = []
 		self.debug = debug
 		self.connect()
 
 	def connect(self):
-		self._connection = serial.Serial('/dev/ttyACM99', self.baud, timeout=5)
+		self._connection = serial.Serial(
+			'/dev/ttyACM99',
+			self.baud,
+			timeout=10,
+			parity= 'N',
+			bytesize=8,
+			stopbits=1
+		)
 		if self._connection.isOpen():
 			self._connection.close()
 		self._connection.open()
@@ -20,8 +27,7 @@ class UartCom(object):
 		if len(data) > MAX_LEDS:
 			raise ValueError("%s exceeds MAX_LEDS" % len(data))
 
-		for idx in range(len(data)):
-			self.data[idx] = data[idx]		
+		self.data = data
 
 	def write(self,val):
 		self._connection.write(val)
@@ -29,30 +35,36 @@ class UartCom(object):
 	def read(self,n):
 		return self._connection.read(n)
 
-	def write_whole_array(self,length = MAX_LEDS):
+	def write_whole_array(self):
 		arr = []
 		if self.debug:
 			print "Writing data..."
+
+		length = len(self.data)
+
 		start = time.time()
+		self.connect()
 		self._connection.write(chr((length >> 8) & 0xff))
 		self._connection.write(chr(length & 0xff))
 
 		for idx in range(length):
 			self._connection.write(chr(self.data[idx]))
+		self._connection.write('\0')
 
 		if self.debug:
 			print "Time to write: %s" % (time.time() - start)
 
-#		arr.append(self._connection.read())
-#
-#		while self._connection.inWaiting() > 0:
-#			arr.append(self._connection.read())
-#
+		arr += [self._connection.read()]
+		
+		arr += self.read(self._connection.inWaiting())
+		print arr
+
 		end = time.time()
 		if self.debug:
 			print "Time taken: %s" % (end - start)
 			print arr
 
+		self.close()
 		return arr
 
 	def close(self):
