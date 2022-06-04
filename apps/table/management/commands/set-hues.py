b62908403f93b7f7c750348cf36bff69009077ea
Tristan Lucas <tristan.lucas.de@gmail.com>
2022-06-04 02:21:18 +0200

    INDEV auto deploy

--- apps/table/management/commands/set-hues.py
+++ apps/table/management/commands/set-hues.py
@@ -7,8 +7,11 @@ class Command(BaseCommand):
     help = "Set sound-to-light colors."
 
     def add_arguments(self, parser):
+        parser.add_argument('-i', '--intensity', default=.7, type=float)
+        parser.add_argument('-f', '--fnc-count', default=60000, type=int)
+        parser.add_argument('-d', '--dim-count', default=1000, type=int)
         parser.add_argument('hues', metavar='HUES', type=float, nargs='*')
 
-    def handle(self, *args, hues=(), **options):
+    def handle(self, *args, intensity=None, fnc_count=None, dim_count=None, hues=(), **options):
         setter = UartSetState(serial_class=PatchedSerial)
-        setter.command(*hues)
+        setter.command(*hues, intensity=intensity, fnc_count=fnc_count, dim_count=dim_count)
