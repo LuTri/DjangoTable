77a36ba2cbdaeb9b591a731e6c70e16cf0c00788
Tristan Lucas <tristan.lucas.de@gmail.com>
2022-06-04 00:31:29 +0000

    WIP set-hues

--- tablehost/uart.py
+++ tablehost/uart.py
@@ -794,10 +794,6 @@ class SoundToLight(UartCom):
 class UartSetState(UartCom):
     CMD = 'CMD_SET_STATE'
 
-    @property
-    def expected_answers(self):
-        return [self.DEFINES.get('MSG_STATE_DATA_STOP').encode(),]
-
 
 class UartGetState(UartCom):
     CMD = 'CMD_GET_STATE'
